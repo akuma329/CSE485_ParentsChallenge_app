b1148e017428c057d964391f674df0dfdb6ac90b
ssdewan <81835258+ssdewan@users.noreply.github.com>
2025-11-23 22:08:10 -0700

    Added part about founders + standards of excellence.
    Founded in 2000 by Steve & Joyce Schuck, Parents Challenge provides families with tools and resources to exercise educational choice to achieve academic success. Parents Challenge has provided educational choice to nearly 4,000 students and their families. Over two million dollars have been disbursed in scholarships and grants to ensure that choice is never limited to only those of means. As they take responsibility for their children’s education, parents have also taken more control of their own lives. Parents Challenge students have prospered, gaining the confidence and skills to succeed in life. 
    
    Twenty-two years of measuring academic performance have taught us that empowered parents and guardians working through the support system of our programs have dramatically improved their lives and those of their children. Through the work of Parents Challenge, families have been transformed and they, as well as our community, are better off as a result.

--- app/about.tsx
+++ app/about.tsx
@@ -1,5 +1,5 @@
 //about us page
-//Last edited: 11/13/2025
+//Last edited: 11/23/2025
 //Edited by: Sheneeza
 
 import React, { useState } from 'react';
@@ -11,6 +11,18 @@ const aboutUs = () => {
   const [titleText, setTitleText] = useState("About Us");
   const bodyText = 'Parents Challenge disrupts the legacy of educational failure by empowering parents. We provide our families with information, training, mentoring, tools, and financial resources to equip them to choose the education they think best for their children.';
   const [backButton, setBackButton] = useState(false);
+  const makingADifference1 = 'Founded in 2000 by Steve & Joyce Schuck, Parents Challenge provides families with tools and resources to exercise educational choice to achieve academic success. Parents Challenge has provided educational choice to nearly 4,000 students and their families. Over two million dollars have been disbursed in scholarships and grants to ensure that choice is never limited to only those of means. As they take responsibility for their children’s education, parents have also taken more control of their own lives. Parents Challenge students have prospered, gaining the confidence and skills to succeed in life. \n\nTwenty-two years of measuring academic performance have taught us that empowered parents and guardians working through the support system of our programs have dramatically improved their lives and those of their children. Through the work of Parents Challenge, families have been transformed and they, as well as our community, are better off as a result.';
+
+  const [standardsOfExcellenceTitle, setstandardsOfExcellenceTitle] = useState("Our Standards of Excellence");
+
+  const standardsOfExcellence = [
+  "Whole-child education that involves the parent, the child, the family dynamic, and external resources.",
+  "Empowerment for parents to advocate on behalf of all children, not just their own.",
+  "Supplementary education services and experimental learning support",
+  "Informational and financial resources for parents to determine the best learning environment for their children.",
+  "Partnerships with parents and their children, built on respect, mutual accountability, and commitment.",
+  "Analysis of students progress to ensure dynamic program effecteness to better serve our families' needs."
+];
 
   return (
     <SafeAreaProvider>
@@ -25,6 +37,25 @@ const aboutUs = () => {
           <Text style={styles.bodyText}>{bodyText}</Text>
         </View>
 
+        <View style={styles.titleContainer}>
+          <Text style={styles.standardsOfExcellenceTitle}>{standardsOfExcellenceTitle}</Text>
+        </View>
+
+        
+
+
+        {standardsOfExcellence.map((item, index) => (
+          <Text key={index} style={styles.standardsOfExcellence}>
+              ‣ {item}
+        </Text>
+
+        
+))}
+
+        <View style={styles.bodyContainer}>
+          <Text style={styles.makingADifference1}>{makingADifference1}</Text>
+        </View>
+
       </SafeAreaView>
     </SafeAreaProvider>
 
@@ -32,11 +63,14 @@ const aboutUs = () => {
   );
 };
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 40,
     paddingHorizontal: 20,
+    backgroundColor: '#ffffff'
   },
   titleContainer: {
     width: '100%',        
@@ -52,7 +86,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#489cbdff', 
     borderRadius: 5,          
     alignSelf: 'stretch',  
-    padding: 20,       
+    padding: 20,      
+    marginBottom: 12, 
   },
 
   bodyText: {
@@ -60,9 +95,25 @@ const styles = StyleSheet.create({
     textAlign: 'left', 
     color: '#ffffff',  
   },
+
+  makingADifference1: {
+  fontSize: 16,
+  color: '#ffffff',
+  textAlign: 'left',
+},
+
+standardsOfExcellenceTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  standardsOfExcellence: {
+    fontSize: 16,
+    color: '#000000ff',
+    marginTop: 8,
+    marginBottom: 12,
+  },
   
 });
 
 export default aboutUs;
-
-
