ed792287631f482a22f68f333934f8851f66ea89
Aiden Meyer <asmeyer5@asu.edu>
2025-12-04 12:21:23 -0700

    Added resources page

--- app/other_resources.tsx
+++ app/other_resources.tsx
@@ -1,9 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as Linking from "expo-linking";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Other_Resources() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Other Resources Page</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() =>
+          Linking.openURL("https://parentschallenge.org/parents/tutoring/")
+        }
+      >
+        <Text style={styles.buttonText}>Tutoring</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.button}
+        onPress={() =>
+          Linking.openURL("https://parentschallenge.org/sports-resources/")
+        }
+      >
+        <Text style={styles.buttonText}>Sports</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.button}
+        onPress={() =>
+          Linking.openURL("https://parentschallenge.org/mental-health-resources/")
+        }
+      >
+        <Text style={styles.buttonText}>Mental Health</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.button}
+        onPress={() =>
+          Linking.openURL("https://parentschallenge.org/parents/resources/")
+        }
+      >
+        <Text style={styles.buttonText}>More Resources</Text>
+      </Pressable>
     </View>
   );
 }
@@ -14,8 +50,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
+  button: {
+    width: 260,
+    backgroundColor: "#6699AB", 
+    paddingVertical: 16,
+    borderRadius: 20,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
