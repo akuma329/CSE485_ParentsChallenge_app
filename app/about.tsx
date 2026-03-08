91b9754f151fc03886e971c4e61f6b9e5733d2c6
ssdewan <81835258+ssdewan@users.noreply.github.com>
2026-03-07 21:04:42 -0700

    Update about.tsx so wen can use it

--- app/about.tsx
+++ app/about.tsx
@@ -1,95 +1,33 @@
 //about us page
-//Last edited: 2/24/2026
+//Last edited: 11/13/2025 
 //Edited by: Sheneeza
 
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, ScrollView, Image } from 'react-native';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const aboutUs = () => {
+const TextInANest = () => {
   const [titleText, setTitleText] = useState("About Us");
-  const [whoWeAreText, setWhoWeAreText] = useState("The Mission");
-  const missionText = 'Parents Challenge disrupts the legacy of educational failure by empowering parents. We provide our families with information, training, mentoring, tools, and financial resources to equip them to choose the education they think best for their children.';
+  const bodyText = 'Parents Challenge disrupts the legacy of educational failure by empowering parents. We provide our families with information, training, mentoring, tools, and financial resources to equip them to choose the education they think best for their children.';
   const [backButton, setBackButton] = useState(false);
 
-  const makingADifference1 = 'Founded in 2000 by Steve & Joyce Schuck, Parents Challenge provides families with tools and resources to exercise educational choice to achieve academic success. Parents Challenge has provided educational choice to nearly 4,000 students and their families. Over two million dollars have been disbursed in scholarships and grants to ensure that choice is never limited to only those of means. As they take responsibility for their children’s education, parents have also taken more control of their own lives. Parents Challenge students have prospered, gaining the confidence and skills to succeed in life. \n\nTwenty-two years of measuring academic performance have taught us that empowered parents and guardians working through the support system of our programs have dramatically improved their lives and those of their children. Through the work of Parents Challenge, families have been transformed and they, as well as our community, are better off as a result.';
-
-  const [standardsOfExcellenceTitle, setstandardsOfExcellenceTitle] = useState("Our Standards of Excellence");
-
-  const standardsOfExcellence = [
-  "Whole-child education that involves the parent, the child, the family dynamic, and external resources.",
-  "Empowerment for parents to advocate on behalf of all children, not just their own.",
-  "Supplementary education services and experimental learning support",
-  "Informational and financial resources for parents to determine the best learning environment for their children.",
-  "Partnerships with parents and their children, built on respect, mutual accountability, and commitment.",
-  "Analysis of students progress to ensure dynamic program effecteness to better serve our families' needs."
-  ];
-   const ourFoudningPrinciplesBullets = [
-  "All children have the right to be educated.",
-  "Parents know what is best for their children.",
-  "Schools must be accountable to the children and their parents.",
-  "Empowering parents with “choice” means a better education for all.",
-  "Parents must be engaged in the education of their children.",
-  "Most importantly, we are committed to making these beliefs real and available to families in Colorado Springs and, ultimately, across the country."
-  ];
-
-  
-  //images
-  const Absenteeism = () => {
-    return (
-      <View style={styles.container}>
-        <Image 
-        style={styles.image}
-        source={require('../assets/images/pc_chronic_absenteeism.png')}
-        />
-      </View>
-      );
-  };
-
-  //display
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
 
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>{titleText}</Text>
-          </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>{titleText}</Text>
+        </View>
 
-          <View style={styles.bodyContainer}>
-            <Text style={styles.makingADifference1}>
-              {makingADifference1}
-            </Text>
-          </View>
+        {/* Rounded rectangle for intro of about page */}
+        <View style={styles.bodyContainer}>
+          <Text style={styles.bodyText}>{bodyText}</Text>
+        </View>
 
-          <View style={styles.titleContainer}>
-            <Text style={styles.whoWeAre}>{whoWeAreText}</Text>
-          </View>
-
-          <View style={styles.titleContainer}>
-            <Text style={styles.missionText}>{missionText}</Text>
-          </View>
-
-          <View style={styles.titleContainer}>
-            <Text style={styles.standardsOfExcellenceTitle}>
-              {standardsOfExcellenceTitle}
-            </Text>
-          </View>
-
-          {standardsOfExcellence.map((item, index) => (
-            <Text key={index} style={styles.standardsOfExcellence}>
-              {item}
-            </Text>
-          ))}
-
-          <View style={styles.titleContainer}>
-            <Absenteeism />
-          </View>
-
-        </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
+
+  
   );
 };
 
@@ -98,66 +36,30 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 40,
     paddingHorizontal: 20,
-    backgroundColor: '#ffffff'
   },
-
-  scrollContainer: {
-    paddingBottom: 40,
-  },
-
   titleContainer: {
-    width: '100%',
-    alignItems: 'center',
+    width: '100%',        
+    alignItems: 'center', 
     marginBottom: 20,
   },
-
   titleText: {
     fontSize: 32,
     fontWeight: 'bold',
   },
 
   bodyContainer: {
-    backgroundColor: '#48a4bdff',
-    borderRadius: 5,
-    alignSelf: 'stretch',
-    padding: 20,
-    marginBottom: 12,
+    backgroundColor: '#489cbdff', 
+    borderRadius: 5,          
+    alignSelf: 'stretch',  
+    padding: 20,       
   },
 
-  missionText: {
+  bodyText: {
     fontSize: 16,
-    textAlign: 'left',
-    color: '#000000ff',
+    textAlign: 'left', 
+    color: '#ffffff',  
   },
-
-  makingADifference1: {
-    fontSize: 16,
-    color: '#ffffff',
-    textAlign: 'left',
-  },
-
-  standardsOfExcellenceTitle: {
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-
-  standardsOfExcellence: {
-    fontSize: 16,
-    color: '#000000ff',
-    marginTop: 8,
-    marginBottom: 12,
-  },
-
-  whoWeAre: {
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-
-  image: {
-    width: 200,
-    height: 200,
-    resizeMode: 'contain',
-  },
+  
 });
 
-export default aboutUs;
+export default TextInANest;
